6f62eb177ca1c95c9c3b1c120a02dae1e8d135f2
otailai <otailai@163.com>
2020-06-10 09:59:19 +0800

    init

---
+++ src/main/java/com/skywalking/seserver/SeServerApplication.java
@@ -0,0 +1,13 @@
+package com.skywalking.seserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skywalking/seserver/SeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skywalking.seserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
